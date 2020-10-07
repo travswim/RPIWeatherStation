dcc1499af8041919f25b2cdfb76fecce96b56f69
Travis Booth <travswim@gmail.com>
2020-10-06 22:06:45 -0700

    wind speed & rg11 work. adjusted debounce time

--- testRG11.py
+++ testRG11.py
@@ -5,13 +5,6 @@ def button_callback(channel):
     global count
     count += 1
     print("Railfall: {:.2f}mm".format(count*0.02))
-# GPIO.setwarnings(False) # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-# GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=1000) # Setup event on pin 10 rising edge
-
-# message = input("Press enter to quit\n\n") # Run until someone presses enter
-# GPIO.cleanup() # Clean up
 
 def RG11():
 
@@ -19,7 +12,7 @@ def RG11():
     # GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=400) # Setup event on pin 10 rising edge
+    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=115) # Setup event on pin 10 rising edge
 
     message = input("Press enter to quit\n\n") # Run until someone presses enter
     # return count
--- testWindSpeed.py
+++ testWindSpeed.py
@@ -1,10 +1,25 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-def button_callback(channel):
-    print("Wind speed: 1 rev")
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(8, GPIO.FALLING, callback=button_callback) # Setup event on pin 10 rising edge
 
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+rotations = 0
+def button_callback(channel):
+    global rotations
+    rotations += 1
+    print("Number of rotations: {0}".format(rotations))
+
+def RG11():
+
+    import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+    # GPIO.setwarnings(False) # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+    GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set initial value to be pulled low (off)
+    GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback, bouncetime=115) # Setup event on pin 8 rising edge
+
+    message = input("Press enter to quit\n\n") # Run until someone presses enter
+    # return count
+    GPIO.cleanup() # Clean up
+
+def main():
+    RG11()
+
+if __name__ == '__main__':
+    main()
