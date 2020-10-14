73648742f4098d75a357e3d9554608c5f775797d
Travis Booth <travswim@gmail.com>
2020-10-13 21:59:13 -0700

    implemented windspeed and rainfall monitoring

--- testRG11.py
+++ testRG11.py
@@ -1,10 +1,40 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-
+from time import sleep
 count = 0
+
+def get_RG11() -> float:
+    """
+    Gets the amount of rainfall since the last reset
+
+    Arguments: None
+
+    Returns: The amount of water fallen in mm
+    """
+    return round(count*0.2, 2)
+
+def reset_RG11():
+    """
+    Resets the counter
+
+    Arguments: None
+
+    Returns: None
+    """
+    global count
+    count = 0
+
 def button_callback(channel):
+    """
+    Callback function to increase the rainfall counter for the RG11
+    
+    Arguments:
+        - channel: the callback channel
+
+    Returns: None
+    """
     global count
     count += 1
-    print("Railfall: {:.2f}mm".format(count*0.02))
+
 
 def RG11():
 
@@ -12,14 +42,36 @@ def RG11():
     # GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback, bouncetime=115) # Setup event on pin 10 rising edge
+    GPIO.add_event_detect(10, GPIO.FALLING, callback=button_callback, bouncetime=115) # Setup event on pin 10 rising edge
 
-    message = input("Press enter to quit\n\n") # Run until someone presses enter
-    # return count
-    GPIO.cleanup() # Clean up
+    while True:
+        sleep(3)
+
+    GPIO.cleanup()
+
+
+def is_any_thread_alive(threads):
+    """
+    Checks if there are any threads running
+
+    Arguments:
+        - threads: A list of threads running
+
+    returns: True if there are any live threads, False otherwise
+    """
+    return True in [t.is_alive() for t in threads]
 
 def main():
-    RG11()
+    """
+    Driver function
+    """
+    import threading
+    run_RG11 = threading.Thread(target=RG11, name="RG11", daemon=True)
+    run_RG11.start()
+    
+    while is_any_thread_alive([run_RG11]):
+        print("Rainfall: " + str(get_RG11()) + "mm")
+        sleep(3)
 
 if __name__ == '__main__':
     main()
--- testWindSpeed.py
+++ testWindSpeed.py
@@ -1,23 +1,111 @@
+from time import sleep
+
+# Global Vars
+wind_speed = 0
+max_wind_speed = float('-inf')
+min_wind_speed = float('inf')
+
 rotations = 0
+
+def get_wind_speed():
+    """
+    Gets the current, maximum, and minimum windspeeds
+
+    Arguments: None
+
+    Returns: a tuple: (wind_speed, max_wind_speed, min_wind_speed)
+    """
+    global wind_speed
+    global max_wind_speed
+    global min_wind_speed
+
+    return round(wind_speed,2), round(max_wind_speed, 2), round(min_wind_speed,2)
+
+def reset_wind_speed():
+    """
+    Resets the maximum and minimum wind speeds
+
+    Arguments: None
+
+    Returns: None
+    """
+    global max_wind_speed
+    global min_wind_speed
+
+    max_wind_speed = float('-inf')
+    min_wind_speed = float('inf')
+
 def button_callback(channel):
+    """
+    Callback function to increase the number of rotations of the anemometer
+    
+    Arguments:
+        - channel: the callback channel
+
+    Returns: None
+    """
     global rotations
     rotations += 1
-    print("Number of rotations: {0}".format(rotations))
 
-def wind_speed():
+def anemometer():
+    """
+    Calculates the anemometer wind speed
 
+    Argumetns: None
+
+    Retruns: None
+    """
     import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
     # GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback, bouncetime=125) # Setup event on pin 8 rising edge
+    # GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback, bouncetime=125) # Setup event on pin 8 rising edge
+    GPIO.add_event_detect(8, GPIO.FALLING, callback=button_callback, bouncetime=125)  
 
-    message = input("Press enter to quit\n\n") # Run until someone presses enter
+
+    # message = input("Press enter to quit\n\n") # Run until someone presses enter
     # return count
+    while True:
+        global rotations
+        global max_wind_speed
+        global min_wind_speed
+        global wind_speed
+
+        rotations = 0
+        sleep(3)
+        wind_speed = round(rotations*0.75*1.609344, 2)
+
+        if wind_speed > max_wind_speed:
+            max_wind_speed = wind_speed
+        if wind_speed < min_wind_speed:
+            min_wind_speed = wind_speed
+
+        
     GPIO.cleanup() # Clean up
 
+def is_any_thread_alive(threads):
+    """
+    Checks if there are any threads running
+
+    Arguments:
+        - threads: A list of threads running
+
+    returns: True if there are any live threads, False otherwise
+    """
+    return True in [t.is_alive() for t in threads]
+
 def main():
-    wind_speed()
+    """
+    Driver function
+    """
+    import threading
+    run_anemometer = threading.Thread(target=anemometer, name="Anemometer", daemon=True)
+    run_anemometer.start()
+    # anemometer()
+    while is_any_thread_alive([run_anemometer]):
+
+        print("rotations: " + str(rotations) + "\t" + str(wind_speed) + "KPH" + "\t" + str(max_wind_speed) + "KPH" + "\t" + str(min_wind_speed) + "KPH")
+        sleep(3)
 
 if __name__ == '__main__':
     main()
