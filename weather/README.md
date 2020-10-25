2ddef35c8b676cd5e46385ddee1d3012a56fbe83
Travis Booth <travswim@gmail.com>
2020-10-24 22:29:52 -0700

    implimented env file, added services directory

--- weather/README.md
+++ weather/README.md
@@ -1,1 +1,8 @@
 # Raspberry Pi Weather Station
+
+
+# Notes
+- Reset SPI and I2C if sensors stop working:
+```bash
+$ sudo raspi-config
+```
---
+++ weather/services/adafruit_io.py
@@ -0,0 +1,18 @@
+from Adafruit_IO import Client, Feed
+import json
+
+
+
+# [ ] TODO: Check if feeds created, if not create feeds
+# [ ] TODO: Upload data to feeds
+
+
+def adafruit_io_startup(key: str, username: str) -> None:
+
+
+
+def adafruit_io_update_feed(feed: str) -> None:
+
+
+if __name__ == "__main__":
+    pass
