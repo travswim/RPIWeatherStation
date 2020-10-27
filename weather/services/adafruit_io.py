56e1b3b363b11127561e851cdd8a96e10cb0ae1e
Travis Booth <travswim@gmail.com>
2020-10-26 23:20:02 -0700

    connected pm2.5, and started testing adafruit IO

--- weather/services/adafruit_io.py
+++ weather/services/adafruit_io.py
@@ -8,11 +8,19 @@ import json
 
 
 def adafruit_io_startup(key: str, username: str) -> None:
+    aio = Client(username, key)
+    feeds = aio.feeds()
+    groups = aio.groups()
+    # print(json.dumps(feeds[0], indent=4))
+    print(feeds[0])
+    print(groups)
+    
 
 
-
-def adafruit_io_update_feed(feed: str) -> None:
+# def adafruit_io_update_feed(feed: str) -> None:
 
 
 if __name__ == "__main__":
-    pass
+    ADAFRUIT_IO_KEY = "travswim"
+    ADAFRUIT_IO_USERNAME = "aio_kCMI35aAHW0TwxWkLaR6tu9bDB4N"
+    adafruit_io_startup(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
