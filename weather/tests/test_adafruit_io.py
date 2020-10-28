7664f77ba1be479ffb03c7ab37c14a486a770a50
Travis Booth <travswim@gmail.com>
2020-10-28 13:14:35 -0700

    implemented tests

--- weather/tests/test_adafruit_io.py
+++ weather/tests/test_adafruit_io.py
@@ -1,6 +1,27 @@
 from Adafruit_IO import Client, Feed, RequestError
+from dotenv import load_dotenv
 import unittest
+from random import randint
 
 class Test_TestAdafruitIO(unittest.TestCase):
     def test_adafruit_io_feed(self):
+        load_dotenv(verbose=True)
+
+        KEY = os.getenv("ADAFRUIT_IO_KEY")
+        USERNAME = os.getenv("ADAFRUIT_IO_USERNAME")
+        aio = Client(username, key)
+
+        try:
+            test = aio.feeds("test")
+        except RequestError: # Doesn't exist, create a new feed
+            feed = Feed(name="test")
+            test = aio.create_feed(feed)
+
+        for i in range(5):
+            value = randint(0, 50)
+            aio.send_data(test.key, value)
+
+        data = aio.receive(test.key)
+        self.assertIsNotNone(data.value)
+
         
--- weather/tests/test_sensors.py
+++ weather/tests/test_sensors.py
@@ -1,9 +1,9 @@
 from weather.sensors.i2c_devices import BME280, PM25
-from weather.sensors.wind_direction import voltage, voltage_to_degrees, voltage_to_direction
-from weather.sensors.wind_speed import button_callback, rotations
-from weather.sensors.RG11 import button_callback as bs, count
-import RPi.GPIO as GPIO
+# from weather.sensors.wind_direction import voltage, voltage_to_degrees, voltage_to_direction
+# from weather.sensors.wind_speed import button_callback, rotations
+# from weather.sensors.RG11 import button_callback as bs, count
 
+import RPi.GPIO as GPIO
 import unittest
 
 class Test_TestSensors(unittest.TestCase):
@@ -48,45 +48,5 @@ class Test_TestSensors(unittest.TestCase):
         
     
 
-
-
- 
-# Create library object using our Bus I2C port
-# i2c = busio.I2C(board.SCL, board.SDA)
-# bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
- 
-# OR create library object using our Bus SPI port
-# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-# bme_cs = digitalio.DigitalInOut(board.D10)
-# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
- 
-# change this to match the location's pressure (hPa) at sea level
-# bme280.sea_level_pressure = 1013.25
-# 
-# while True:
-#     print("\nTemperature: %0.1f C" % bme280.temperature)
-#     print("Humidity: %0.1f %%" % bme280.humidity)
-#     print("Pressure: %0.1f hPa" % bme280.pressure)
-#     print("Altitude = %0.2f meters" % bme280.altitude)
-#     time.sleep(2)
-
-# def BME():
-#     import board
-#     import busio
-#     import adafruit_bme280
-#     # Create library object using our Bus I2C ports
-#     i2c = busio.I2C(board.SCL, board.SDA)
-#     bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-
-#     return bme280.temperature, bme280.humidity, bme280.pressure
-
-# def main():
-#     while True:
-#         temperature, humidity, pressure = BME()
-#         print("\nTemperature: %0.1f C" % temperature)
-#         print("Humidity: %0.1f %%" % humidity)
-#         print("Pressure: %0.1f hPa" % pressure)
-#         sleep(2)
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == "__main__":
+    unittest.main()
