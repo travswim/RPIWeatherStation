6be7396c03579f9dc36a98510a3cb0980e400400
Travis Booth <tmbooth@sfu.ca>
2020-11-05 13:58:45 -0800

    updated bash script start app onr restart

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Install python and upgrade raspbian
 sudo apt update && sudo apt upgrade -y
-
 sudo apt install python3
 sudo apt install python3-pip
 
+# Dependencies
 pip3 install -r requirements.txt
 
-nohup python3 -m weather &
+# Run weather station app on restart
+sudo cp weather.service /etc/systemd/system/weather.service
+sudo reboot
