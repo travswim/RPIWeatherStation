91100367464932446dd357fed09f8d64bf4eb5e7
Travis Booth <tmbooth@sfu.ca>
2020-11-08 19:22:30 -0800

    automated install script

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Install python and upgrade raspbian
 sudo apt update && sudo apt upgrade -y
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 
 # Dependencies
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -y
 
 # Run weather station app on restart
 sudo cp weather.service /etc/systemd/system/weather.service
